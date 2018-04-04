aec6cf9041230826379c115ab93792415d7b1f2e
julesjagers <jules.ajax@hotmail.com>
2018-04-04 09:54:05 +0200

    tweede keer dat ik probeer te pushen

---
+++ AppleWatchDemo/AppleWatchDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleWatchDemo
+//
+//  Created by Jules Jagers on 04-04-18.
+//  Copyright © 2018 Jules Jagers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
