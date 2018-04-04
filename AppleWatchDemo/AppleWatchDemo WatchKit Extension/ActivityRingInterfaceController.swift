d1a16313bbc236f4181db2bba49eecb9be727b62
julesjagers <jules.ajax@hotmail.com>
2018-04-04 13:14:23 +0200

    2 onderdelen toegevoegd

--- AppleWatchDemo/AppleWatchDemo WatchKit Extension/ActivityRingInterfaceController.swift
+++ AppleWatchDemo/AppleWatchDemo WatchKit Extension/ActivityRingInterfaceController.swift
@@ -1,5 +1,5 @@
 //
-//  InterfaceController.swift
+//  ActivityRingInterfaceController.swift
 //  AppleWatchDemo WatchKit Extension
 //
 //  Created by Jules Jagers on 04-04-18.
@@ -8,24 +8,33 @@
 
 import WatchKit
 import Foundation
+import HealthKit
 
+class ActivityRingInterfaceController: WKInterfaceController {
 
-class InterfaceController: WKInterfaceController {
-    
+    @IBOutlet var activityRing: WKInterfaceActivityRing!
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
     }
-    
-    override func willActivate() {
+
+    override func willActivate(){
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+
+
+        
+        let activitySummary: HKActivitySummary = HKActivitySummary()
+        activitySummary.activeEnergyBurned = HKQuantity(unit: HKUnit.smallCalorie(), doubleValue: 100.0)
+        
+        activityRing.setActivitySummary(activitySummary, animated: true)
+        
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
 }
--- AppleWatchDemo/AppleWatchDemo WatchKit Extension/MenuInterfaceController.swift
+++ AppleWatchDemo/AppleWatchDemo WatchKit Extension/MenuInterfaceController.swift
@@ -1,5 +1,5 @@
 //
-//  InterfaceController.swift
+//  MenuInterfaceController.swift
 //  AppleWatchDemo WatchKit Extension
 //
 //  Created by Jules Jagers on 04-04-18.
@@ -10,19 +10,19 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
-    
+class MenuInterfaceController: WKInterfaceController {
+
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
